5da623c447e3d094c7254d3b2d01952d1d4c0e7e
Alan Rogers <alanjrogers@me.com>
2025-05-12 12:23:01 +1000

    Make a new components that will paginate

---
+++ src/components/ui/images.tsx
@@ -0,0 +1,56 @@
+import {Character, CharacterQueryResponse} from '@/lib/types';
+import {Button, Card, Grid} from '@chakra-ui/react';
+import {useSuspenseQuery} from '@apollo/client';
+import {characterQuery} from '@/lib/data';
+import Image from 'next/image';
+
+export const Images = () => {
+  const {error, data, fetchMore} = useSuspenseQuery<CharacterQueryResponse>(
+    characterQuery,
+    {
+      variables: {
+        offset: 0,
+        limit: 10,
+      },
+    }
+  );
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <Grid
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(4, 1fr)',
+          lg: 'repeat(6, 1fr)',
+        }}
+        gap="4"
+      >
+        {data.characters.results.map((character: Character, index: number) => (
+          <Card.Root key={index}>
+            <Card.Header>
+              <Image
+                src={character.image}
+                width={300}
+                height={300}
+                alt={character.name}
+              />
+              <Button>{character.status}</Button>
+              <Card.Title>{character.name}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <p>{character.species}</p>
+              <p>Origin: {character.origin.name}</p>
+              <p>Location: {character.location.name}</p>
+            </Card.Body>
+          </Card.Root>
+        ))}
+      </Grid>
+      <Button onClick={() => fetchMore({})}>Fetch more</Button>
+    </>
+  );
+};
